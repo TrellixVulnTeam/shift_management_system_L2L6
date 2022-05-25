cee99206437c268ed82b50e8250c2a26ff5e7a95
pirlodog1125 <59393354+pirlodog1125@users.noreply.github.com>
2022-05-25 16:28:31 +0900

    Update CreateLambdaPrepareUserPool.ts

--- shift_management_system/lib/CreateLambdaPrepareUserPool.ts
+++ shift_management_system/lib/CreateLambdaPrepareUserPool.ts
@@ -6,16 +6,16 @@ import { DataSourceStackProps } from './data-source-stack'
 
 export default class CreateLambdaPrepareUserPool {
   public static execute(stack: cdk.Stack, userPool: cognito.UserPool, lambdaEnvironment: {}, props: DataSourceStackProps):lambda.Function  {
-		const lambdaFunction: lambda.Function = new lambda.Function(stack, `viewer-prepare-user-pool-${props.isAdmin ? "admin" : "user"}`, {
-			runtime: lambda.Runtime.NODEJS_12_X,
-			code: lambda.Code.fromAsset('src/data_source/custom_resource/lambda/ViewerPrepareUserPool/deployment_package'),
-			handler: 'index.handler',
-			environment: lambdaEnvironment,
-			functionName: `${props.projectName}-prepare-user-pool-${props.isAdmin ? "admin" : "user"}`,
-			description: `Cognitoユーザープール${userPool.userPoolProviderName}の追加設定を行う。-${props.isAdmin ? "admin" : "user"}`,
-			timeout: cdk.Duration.minutes(3),
-		});
-    lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
+	const lambdaFunction: lambda.Function = new lambda.Function(stack, `viewer-prepare-user-pool-${props.isAdmin ? "admin" : "user"}`, {
+		runtime: lambda.Runtime.NODEJS_12_X,
+		code: lambda.Code.fromAsset('src/data_source/custom_resource/lambda/ViewerPrepareUserPool/deployment_package'),
+		handler: 'index.handler',
+		environment: lambdaEnvironment,
+		functionName: `${props.projectName}-prepare-user-pool-${props.isAdmin ? "admin" : "user"}`,
+		description: `Cognitoユーザープール${userPool.userPoolProviderName}の追加設定を行う。-${props.isAdmin ? "admin" : "user"}`,
+		timeout: cdk.Duration.minutes(3),
+	});
+	lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
 		effect: iam.Effect.ALLOW,
 		resources: [
 		  userPool.userPoolArn,
@@ -24,6 +24,6 @@ export default class CreateLambdaPrepareUserPool {
 			'cognito-idp:UpdateUserPool',
 		],
 		}))
-    return lambdaFunction
+	return lambdaFunction
   }
 }
